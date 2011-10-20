ac3dd8b9d439e991f2db4ea71e7a9c442a00923c
Nicos Karalis <dymytrhy@gmail.com>
2011-10-20 09:46:06 -0200

    Added rails squeleton
    Signed-off-by: Nicos Karalis <dymytrhy@gmail.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Contabil::Application.config.secret_token = '2082c80719bdbb66fe17b6f5094568b7c9de12c718705b20a60f6b98a6c9e2295ae7be20a9555edd2ca2eda8db7e6083ddfb458caeb3f0bd77b866942e93cc2e'
